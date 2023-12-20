1871dc446c8c3cd1ec3671fb24703ba9772af0b9
Chase Reynders <chase.reynders@yale.edu>
2023-12-19 23:16:08 -0500

    no more admins

--- backend/user_app/urls.py
+++ backend/user_app/urls.py
@@ -1,12 +1,11 @@
 #user_app.urls
 from django.urls import path
-from .views import Sign_up, Log_in, Log_out, Admin_sign_up, All_users, A_user, Validate, Resend
+from .views import Sign_up, Log_in, Log_out, All_users, A_user, Validate, Resend
 
 urlpatterns = [
     path('signup/', Sign_up.as_view(), name='signup'),
     path("login/", Log_in.as_view(), name="login"),
     path("logout/", Log_out.as_view(), name="logout"),
-    path('admin-signup/', Admin_sign_up.as_view(), name='master'),
     path("", All_users.as_view(), name="all users"),
     path("me/", A_user.as_view(), name="my info"),
     path("<int:userid>/", A_user.as_view(), name="a user"),
