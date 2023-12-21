30b37f95bc42dc49b5d6cc092a7a0a369b0d8962
Chase Reynders <chase.reynders@yale.edu>
2023-12-21 02:38:00 -0500

    latest

--- frontend/src/utilities.jsx
+++ frontend/src/utilities.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1/",
+  baseURL: "https://chaser164.pythonanywhere.com/api/v1/",
 });
 
 // For testing: http://127.0.0.1:8000/api/v1/
