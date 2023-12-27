7d7d431d9fb0b640f9baf719978b2dea617709fe
Chase Reynders <chase.reynders@yale.edu>
2023-12-27 12:23:14 -0500

    added wildcard

--- backend/readout/settings.py
+++ backend/readout/settings.py
@@ -141,7 +141,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173', #development server
-    'https://yale-menus-scraper.netlify.app/',
+    'https://yale-menus-scraper.netlify.app/*',
 ]
 
 # cookies settings
